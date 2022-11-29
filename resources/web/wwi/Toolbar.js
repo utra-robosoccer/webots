740efde14f51a3e85bf3b6496d3a1a6506b34b42
Léo Duggan <leo.duggan@alumni.epfl.ch>
2022-11-29 11:06:24 +0100

    Make robot window visible for benchmarks

--- resources/web/wwi/Toolbar.js
+++ resources/web/wwi/Toolbar.js
@@ -572,6 +572,8 @@ export default class Toolbar {
 
     this.#checkWindowBoundaries();
     this.#initializeWindowLayerChanges();
+    if (new URL(document.location.href).searchParams.get('type') === 'benchmark')
+      this.#changeFloatingWindowVisibility('robot');
   }
 
   #initializeWindowLayerChanges() {
